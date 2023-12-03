e2667a779fdfde583d35be270d65de6efb679414
GoodGuyGregory <greg.witt625@gmail.com>
2023-12-03 00:53:44 -0500

    adds vectorizer and decision tree training data

---
+++ README.md
@@ -0,0 +1,11 @@
+## Authorship
+
+
+
+
+**Resources**
+
+[Text Files in SciKitLearn ](https://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html)
+
+[TfidfVectorizer](https://scikit-learn.org/stable/modules/generated/sklearn.feature_extraction.text.TfidfVectorizer.html#sklearn.feature_extraction.text.TfidfVectorizer)
+[Comparisons in SciKitLearn Performance with Text Vectors](https://scikit-learn.org/stable/auto_examples/text/plot_hashing_vs_dict_vectorizer.html#sphx-glr-auto-examples-text-plot-hashing-vs-dict-vectorizer-py)
--- authorship.py
+++ authorship.py
@@ -1,16 +1,70 @@
 import pandas as pd
 import re
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.tree import DecisionTreeClassifier
 
-def readAusten():
-# read in the files from Austen, divide paragraphs into an array of individual values.
-    northanger = open('austen-northanger-abbey.txt', encoding="utf8").read().split('\n\n')
-    for line in range(len(northanger)):
-        northanger[line] = re.sub('\n',' ', northanger[line])
-    print(northanger)
 
 def main():
-    print('Running')
-    readAusten()
+    textData = {'text': [], 'label':[]}
+
+    # read in the files from Austen, divide paragraphs into an array of individual values.
+    northanger = open('austen-northanger-abbey.txt', encoding="utf8").read().split('\n\n')
+    for line in range(len(northanger)):
+        northanger[line] = re.sub('\n', ' ', northanger[line])
+        textData['text'].append(northanger[line])
+        textData['label'].append(0)
+
+
+    pridePredjudice = open('austen-pride-and-prejudice.txt', encoding="utf8").read().split('\n\n')
+    for line in range(len(pridePredjudice)):
+            pridePredjudice[line] = re.sub('\n', ' ', pridePredjudice[line])
+            textData['text'].append(pridePredjudice[line])
+            textData['label'].append(0)
+
+    frankenstein = open('shelley-frankenstein.txt', encoding="utf8").read().split('\n\n')
+    for line in range(len(frankenstein)):
+        frankenstein[line] = re.sub('\n',' ',frankenstein[line])
+        textData['text'].append(frankenstein[line])
+        textData['label'].append(1)
+
+    theLastman = open('shelley-the-last-man.txt', encoding="utf8").read().split('\n\n')
+    for line in range(len(theLastman)):
+        theLastman[line] = re.sub('\n', ' ', theLastman[line])
+        textData['text'].append(theLastman[line])
+        textData['label'].append(1)
+
+    # build a dataFrame to store all vector data from both authors with unique features
+    df = pd.DataFrame(textData)
+
+    #  train with 70% of overall data and 30% testing
+    X_train, X_test, y_train, y_test = train_test_split(df['text'], df['label'], test_size=0.3)
+
+    vectorizer = TfidfVectorizer(max_df=0.01, max_features=50)
+    X_train_vectorizer = vectorizer.fit_transform(vectorizer)
+    X_test_vectorizer = vectorizer.transform(X_test)
+
+    decisionTree = DecisionTreeClassifier()
+    decisionTree.fit(X_train_vectorizer,y_train)
+
+    # run a prediction
+    y_pred = decisionTree.predict(X_test_vectorizer)
+
+    # get tfidfVectorizer for each author
+    # austenList = northanger + pridePredjudice
+    #
+    # shellyList = frankenstein + theLastman
+    #
+    # vectorizerAusten = TfidfVectorizer(max_df=0.01, max_features=50)
+    # austenFeatures = vectorizerAusten.fit_transform(austenList)
+    # print(vectorizerAusten.get_feature_names_out())
+    # print(austenFeatures.shape)
+    #
+    # vectorizerShelly = TfidfVectorizer(max_df=0.01,max_features=50)
+    # shellyFeatures = vectorizerShelly.fit_transform(shellyList)
+    # print(shellyFeatures.shape)
+    # print(vectorizerShelly.get_feature_names_out())
+
 
 
 
